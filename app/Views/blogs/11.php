c03e6fcc8a98a27607ded98bf615ade3750fe5d3
Daniel Tri <danielrudianto12@gmail.com>
2021-10-17 00:27:41 +0700

    17 Oktober 2021

---
+++ app/Views/blogs/11.php
@@ -0,0 +1,76 @@
+<?= $this->extend('/blog') ?>
+
+<?= $this->section('content') ?>
+    <h1 class='text-center'><?= $header['title'] ?></h1>
+    <img src='<?= base_url('assets/blog') . '/' . $header['id'] . '.webp' ?>' alt='<?= $header['title'] ?>' class='w-100' />
+    <h2 class='text-center h5'><?= $header['subtitle'] ?></h2>
+    <p class='text-center h5'><?= date_format(date_create($blog["created_date"]), "d M Y") ?></p>
+    <hr>
+    <br>
+    <p>Saat ini, penggunaan website sudah menjadi hal yang lumrah bagi perusahaan. Dulu, belum banyak perusahaan yang menggunakan website karena memerlukan keahlian khusus dan biaya. Nyatanya manfaat website bagi perusahaan sangat efektif. Bukan hanya sebagai media informasi dan promosi, namun juga mendekatkan perusahaan dengan masyarakat.</p>
+    <p>Akhir-akhir ini semakin banyak perusahaan yang membangun website-nya sendiri. Bahkan tidak jarang yang mempekerjakan pengembang website secara khusus. Zaman kecanggihan internet membuat banyak orang mengakses berbagai keperluan via online. Tidak heran website menjadi sasaran utama untuk mendapatkan informasi dari sebuah perusahaan. Agar lebih yakin, berikut manfaat website secara lengkap untuk perusahaan:</p>
+    <ol>
+        <li>
+            <h2 class='h5 mt-5'>Meningkatkan Kepercayaan Calon Customer</h2>
+            <p>Manfaat website bagi perusahaan yang pertama terkait dengan kepercayaan. Saat mencari informasi seputar perusahaan, lebih banyak orang merasa yakin dengan perusahaan yang ada website resminya. Apalagi jika website tersebut memiliki informasi yang lengkap dan tersusun dengan baik.</p>
+            <p>Masifnya perkembangan internet membuat banyak orang melakukan berbagai aktivitas secara online. Terlebih saat situasi pandemi yang membuat banyak hal dikerjakan dengan bantuan internet, tidak terkecuali mencari informasi produk. Tanpa adanya website, terdapat kemungkinan calon pelanggan/konsumen beralih pada perusahaan serupa dengan situs resmi.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Sebagai Sarana Pemasaran</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/Marketing.png') ?>' alt='Ilustrasi konsep pemasaran' class='w-100'>
+            </figure>
+            <p>Pemasaran suatu produk ataupun jasa bisa dilakukan secara efektif melalui website. Dari sinilah perusahaan berkesempatan memperkenalkan produknya ke dunia online. Pemakai internet sangat masih, sehingga peluang memasarkan barang akan lebih banyak dilihat daripada secara offline. Semakin tinggi exposure, peluang meraih pelanggan juga bertambah.</p>
+            <p>Keuntungan memanfaatkan website sebagai platform dalam memasarkan produk yaitu banyaknya ruang yang tersedia. Lain halnya dengan memasarkan dari iklan koran, brosur, atau reklame yang memiliki keterbatasan ruang. Website juga lebih mudah untuk diperbarui jika ada yang keliru ataupun kurang. Informasi yang dicantumkan di website bisa lebih mendetail seputar produk hingga jasa yang ditawarkan.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Memperbesar Market</h2>
+            <p>Melalui website, perusahaan berpeluang besar untuk meningkatkan market (pasar)nya. Sebagai contoh, perusahaan yang biasanya menjangkau daerah sekitar wilayahnya saja kemudian terjun ke dunia online melalui website. Terlebih, mengakses internet merupakan aktivitas mudah yang bisa dilakukan kapan saja sambil melakukan hal lain sekalipun.</p>
+            <p>Dengan mengenalkan perusahaan via website, akan lebih banyak orang yang berkunjung dan meningkatkan exposure-nya. Jangkauan market akan lebih luas, sehingga produk maupun jasa yang ditawarkan lebih mudah untuk diketahui masyarakat. Tidak jarang ada yang hingga diakses oleh pengguna luar negeri.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Menunjukkan Profil Perusahaan</h2>
+            <p>Ciri khas suatu perusahaan dapat terlihat dari karakteristik website yang ditampilkannya. Manfaat website bagi perusahaan yang satu ini berkaitan dengan identitas, misalnya logo. Peran logo penting dalam membangun ciri yang akan dikenali oleh pengunjung website. Hal ini bisa dimanfaatkan untuk membangun kesan pertama bagi calon konsumen/pelanggan. Gambaran perusahaan inilah yang menjadi bahan pertimbangan sebelum memutuskan pilihan.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Media Informasi Terbaru</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/Information.png') ?>' alt='Ilustrasi media informasi' class='w-100'>
+            </figure>
+            <p>Website dapat menjadi sumber informasi yang lebih cepat karena dapat diperbarui setiap saat. Daripada mengeluarkan biaya lebih untuk memasang spanduk, poster, dan sejenisnya, website lebih efektif untuk memberikan informasi. Mulai dari jadwal aktivitas, berita terbaru, promosi, dan sebagainya. Buat pengaturan yang mudah dipahami sehingga pengunjung bisa mengakses berbagai informasi.</p>
+            <p>Perkembangan ini bisa diupdate dengan rutin sehingga pelanggan dapat melihatnya hanya berbekal gadget. Apabila sering mengadakan promosi yang menarik dan terus diperbarui, calon konsumen akan melihat adanya konsistensi dan bisa tertarik. Sekarang ini lebih banyak konsumen yang lebih suka berkomunikasi dengan sebuah perusahaan melalui website karena praktis.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Meminimalisir Biaya Layanan</h2>
+            <p>Pertimbangan biaya merupakan salah satu manfaat website bagi perusahaan. Saat berjualan di marketplace, biasanya akan ada biaya tambahan yang dikenakan pada pemilik usaha. Hal ini tentu membebankan customer meski jumlahnya tidak banyak. Melalui pembelian dari website milik sendiri, perusahaan tidak perlu merisaukan hal ini.</p>
+            <p>Perusahaan dapat mengelola website dengan maksimal, terutama di bagian pembayaran sehingga memudahkan pelanggan untuk membeli produk ataupun jasa. Tanpa biaya tambahan, customer akan lebih tertarik untuk melakukan transaksi. Buatlah sistem transaksi yang sederhana namun jelas sehingga meningkatkan rasa percaya customer saat bertransaksi. Pastikan memberi konfirmasi sehingga terjalin kepercayaan antar kedua belah pihak.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Riset Tipe Customer</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/Research.png') ?>' alt='Ilustrasi riset' class='w-100'>
+            </figure>
+            <p>Riset merupakan bagian penting dalam menjalankan sebuah usaha. Website bisa menjadi lahan yang tepat untuk mendapatkan data ini dari jumlah pengunjung yang mengakses situs. Dengan mengetahui tipe customer, perusahaan dapat memfokuskan produknya sesuai karakteristik tertentu. Hal ini berperan dalam meningkatkan penjualan.</p>
+            <p>Website bisa dimanfaatkan untuk menggali seperti apa persona dari customer. Memanfaatkan tools bernama Google Analytic untuk menggali data ini. Misalnya perkiraan usia, jenis kelamin, wilayah, pendidikan, dan sebagainya. Data ini sangat berguna bagi perusahaan untuk dianalisis, selanjutnya digunakan untuk melakukan penyesuaian.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Sarana Edukasi</h2>
+            <p>Website resmi dapat digunakan oleh perusahaan untuk memberikan edukasi bagi pengunjung. Sebagai contoh, perusahaan skincare bisa memberikan tips tentang merawat kulit secara rutin. Pengunjung website akan lebih betah dan merasa tercerahkan dengan adanya manfaat yang didapatkan. Rutin memberikan edukasi dapat menaikkan traffic website.</p>
+            <p>Apabila pengunjung merasa mendapatkan manfaat, kemungkinan besar akan kembali mengunjungi website di lain waktu. Informasi tersebut bisa juga dibagikan pada orang lain misalnya ke sosial media. Secara tidak langsung, kecenderungan ini akan menaikkan jumlah pengunjung website. Manfaatkan fungsi ini untuk mengenalkan produk serta jasa yang ditawarkan.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Meningkatkan Kualitas Layanan</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/MarketingSupport.png') ?>' alt='Ilustrasi marketing support' class='w-100'>
+            </figure>
+            <p>Manfaat website bagi perusahaan berkorelasi dengan meningkatnya kualitas dari layanan. Perusahaan dapat mempekerjakan customer service atau analis data yang bertugas mengumpulkan data dari layanan yang tersedia. Fitur ini bisa dihadirkan dalam bentuk form, chat live, dan sebagainya sesuai kebutuhan website.</p>
+            <p>Dari website, perusahaan juga bisa melakukan survei yang dapat diisi oleh pelanggan. Nantinya data ini bisa digunakan untuk memperbaiki performa perusahaan. Dengan meningkatkan kualitas perusahaan, kepuasan pelanggan juga dapat meningkat. Hal ini berpengaruh terhadap pembelian produk, repeat order, rekomendasi, testimoni, serta tentunya pendapatan perusahaan.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Mendatangkan Partner Baru</h2>
+            <p>Saat ini, tidak jarang perusahaan mendapatkan tawaran kerjasama dari pihak lain setelah melihat website-nya. Tampilan website yang dirancang dengan menarik dan informatif berpotensi untuk mendatangkan minat dari perusahaan lainnya. Dengan publikasi rutin, perusahaan bisa membangun reputasi yang baik.</p>
+            <p>Potensi akan datangnya kerjasama bisa didapatkan melalui website berkualitas dengan publikasi secara aktif. Tidak perlu menunggu datangnya tawaran, perusahaan juga bisa mengajak pihak lain untuk bekerjasama. Sebelum menerima kerjasama, tentu pihak lain akan meneliti terlebih dahulu perusahaan termasuk website-nya.</p>
+        </li>
+    </ol>
+    <p>Itulah beberapa manfaat website bagi perusahaan yang sayang jika dilewatkan. Website menjadi objek potensial untuk mengajak lebih banyak orang mengenal sebuah perusahaan. Penggunaan website termasuk dalam kerja cerdas, karena menjangkau banyak orang tanpa harus mendatangi berbagai tempat. Terlebih, website bisa di-update kapan saja selama ada koneksi internet. Memanfaatkan kehadiran internet untuk menghasilkan performa maksimal bagi perusahaan. <a href='<?= site_url() ?>'>Xunny.id</a> dapat membantu anda untuk mewujudkan keinginan anda untuk membuat website company profile. Kontak kami <a href='<?= base_url() ?>/#Contact'>disini</a></p>
+<?= $this->endSection() ?>
