174e95530630b59cb08be7086776acd7668bd6a7
Daniel Tri <danielrudianto12@gmail.com>
2021-10-19 07:26:21 +0700

    19 Oktober 2021

---
+++ app/Views/blogs/13.php
@@ -0,0 +1,79 @@
+<?= $this->extend('/blog') ?>
+
+<?= $this->section('content') ?>
+    <h1 class='text-center'><?= $header['title'] ?></h1>
+    <img src='<?= base_url('assets/blog') . '/' . $header['id'] . '.webp' ?>' alt='<?= $header['title'] ?>' class='w-100' />
+    <h2 class='text-center h5'><?= $header['subtitle'] ?></h2>
+    <p class='text-center h5'><?= date("d M Y", strtotime($header['created_date'])) ?></p>
+    <hr>
+    <br>
+    <p>Pada dasarnya dalam menjalankan sebuah usaha, tata pengelolaan terkait keuangan memang bukan perkara yang mudah. Bukan tidak mungkin jika salah perhitungan bisa berdampak pada kerugian yang dialami oleh pelaku usaha tersebut. Maka keberadaan dari aplikasi keuangan perusahaan penting untuk membantu perusahaan dalam melakukan perhitungan keuangan yang dimiliki.</p>
+    <figure>
+        <img src='<?= base_url() ?>/assets/blogContent/FinanceReport.png' alt='Ilustrasi laporan keuangan' class='w-100'>
+    </figure>
+    <p>Mengingat sejumlah pelaku usaha memiliki pendapat bahwasanya perhitungan keuangan dengan memakai cara manual dinilai kurang mencapai target yang diinginkan. Bentuk aplikasi itu sendiri adalah sebuah inovasi baru yang tengah berkembang di masa modern ini. Untuk itu para pelaku usaha perlu mengetahui manfaat dari adanya aplikasi keuangan bagi perusahaan yang dimiliki seperti berikut:</p>
+    <ol>
+        <li>
+            <h2 class='h5 mt-5'>Untuk Menghemat Waktu</h2>
+            <p>Daftar manfaat pertama yang bisa para pelaku usaha dapatkan ketika memilih untuk menggunakan aplikasi keuangan perusahaan bagi usahanya yakni lebih hemat waktu. Mengingat sejumlah aplikasi tersebut telah dibekali dengan teknologi yang cukup canggih untuk memberikan bantuan kepada pengusaha dalam menghitung bagian keuangannya.</p>
+            <p>Beberapa kelebihan ditawarkan oleh aplikasi semacam ini. Diantaranya seperti sifatnya yang ramah terhadap pengguna. Hingga sejumlah informasi yang dibutuhkan oleh pengusaha akan mudah didapatkan nantinya. Di samping itu software keuangan biasanya juga menawarkan sejumlah fitur guna lebih mempermudah pengusaha dalam melakukan perhitungan yang cukup transparan.</p>
+            <p><a href='<?= base_url() ?>'>Xunny.id</a> memperhatikan bagaimana sebuah aplikasi <i>real time</i> dapat membantu anda menghemat waktu dan meningkatkan keramahan terhadap pengguna (<i>user friendly</i>) dalam memantau keuangan dengan implementasi <a href='<?= base_url() ?>/Blogs/aplikasi-real-time-dengan-websocket'>web socket</a>.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Untuk Memantau Keuangan</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/monitorFinance.png' alt='Ilustrasi monitor keuangan' class='w-100'>
+            </figure>
+            <p>Manfaat lain ketika menggunakan aplikasi keuangan perusahaan adalah bisa membantu para pelaku usaha untuk melakukan pemantauan terhadap kondisi keuangan usaha. Secara umum penggunaan catatan berupa buku perhitungan secara manual biasanya mayoritas pekerjaan hanya dijalankan pada akhir bulan maupun tahun saja. Yakni saat dimana laporan yang berhubungan dengan administrasi hendak dibutuhkan.</p>
+            <p>Akan tetapi dengan menggunakan kecanggihan teknologi berupa aplikasi dalam menjalankan tugas akuntan ini maka pemantauan keuangan bisa dilakukan kapan pun juga secara terus-menerus. Dengan begitu pelaku usaha bisa memperoleh laporan terkait keuangan dengan lebih mendalam serta bisa melakukan penyelesaian terkait keuangan dalam waktu lebih cepat.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Untuk Memperkecil Kemungkinan Terjadinya Kesalahan</h2>
+            <p>Sistem perhitungan yang bersifat manual memiliki kemungkinan terjadinya kesalahan yang tergolong lebih besar, seperti terjadinya human error. Penyebab human error tersendiri tergolong cukup beragam, bisa lantaran bekerja dengan cara terburu-buru sampai dengan bekerja dalam keadaan tubuh yang kurang sehat serta sebab lainnya.</p>
+            <p>Guna menghindari perkara hal yang semacam ini para pelaku usaha bisa memanfaatkan keberadaan dari aplikasi keuangan perusahaan yang sekarang banyak jenisnya. Nantinya pengusaha berkemungkinan untuk melakukan perhitungan keuangan dari usaha yang dimiliki dengan cara otomatis sehingga hasil yang diperoleh terbilang lebih akurat.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Faktur Yang Terkomputerisasi</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/InvoiceTemplate.png' alt='Ilustrasi faktur yang terkmoputerisasi' class='w-100'>
+            </figure>
+            <p>Diketahui bahwasanya adanya perangkat seperti aplikasi keuangan perusahaan yang memiliki fokus pada bagian pembuatan faktur. Maka pelaku usaha mampu untuk memperoleh laporan berupa pembayaran yang dilakukan oleh para pelanggan maupun laporan terbaru lainnya. Hingga dengan hal ini pelaku usaha bisa mengetahui kiranya pelanggan siapa saja yang berhutang.</p>
+            <p>Selain itu pengusaha juga bisa mengetahui besaran hutang yang dimiliki oleh pelanggan tersebut tanpa bersusah payah mencari buku catatannya. Tentunya perkara semacam ini bisa membuat pengusaha bisa melakukan kontrol terhadap bidang keuangan dari bisnis yang dimiliki dengan cara lebih efektif. Secara singkatnya aplikasi ini cukup bisa diandalkan untuk membantu pengusaha dalam meningkatkan keuntungan.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Lebih Terorganisir</h2>
+            <p>Jika dilihat secara umum, menggunakan buku catatan terbilang kurang efektif lantaran kemungkinan untuk hilang serta salah tempat cukup bisa saja terjadi. Lain halnya dengan menggunakan aplikasi keuangan perusahaan yang memungkinkan para pelaku usaha bisa menjalankan beberapa penyimpan data maupun informasi dalam satu tempat yang lebih terorganisir.</p>
+            <p>Dimana nantinya pengusaha bisa melakukan kegiatan seperti mencetak, lalu mengirimkan data atau catatan tersebut dengan mudah apabila dibutuhkan. Di samping itu pencarian suatu data juga lebih cepat dan gampang dilakukan. Pasalnya pengusaha hanya perlu mengetikkan nama maupun nomornya saja pada kolom pencarian yang telah disediakan.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Membantu Melacak Inventaris</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/MonitorInventory.png' alt='Ilustrasi monitor inventaris' class='w-100'>
+            </figure>
+            <p>Sejumlah keuntungan yaitu beragam informasi bisa pengusaha peroleh dengan berbekal software keuangan berkualitas yang saat ini banyak digunakan. Diantaranya seperti fitur penyimpanan data terkait inventaris suatu produk maupun untuk kebutuhan melacaknya. Dengan begitu pelaku usaha akan lebih mudah dalam mengetahui informasi tentang jumlah stok terbaru produknya.</p>
+            <p>Nilai lebihnya lagi dari aplikasi semacam ini dalam bisa dimanfaatkan untuk kebutuhan pelacakan inventaris bahkan ketika produk tengah dalam masa proses. Hingga pengguna bisa lebih mudah dalam memberikan perkiraan tentang beberapa hal terkait usahanya. Misalnya seperti informasi tentang waktu stok yang bakal habis, sehingga pengusaha bisa melakukan pemesanan sebelum barang benar-benar kosong.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Lebih Mudah Melakukan Perhitungan</h2>
+            <p>Berbekal software keuangan membuat pengusaha bisa lebih mudah dalam melakukan sejumlah kegiatan yang berhubungan dengan keuangan perusahaannya. Misalnya seperti mendapat informasi terkait perhitungan gambaran biaya operasional yang lebih rinci serta kapan perkiraan ada pemasukan serta berapa modal yang dibutuhkan dan lainnya.</p>
+            <p>Dengan begitu pelaku usaha akan lebih gampang dalam melakukan perhitungan keuangan yang akan dimiliki dari hasil usaha tersebut. Selain itu pengusaha juga bisa mengetahui pelanggan siapa saja yang mendatangkan pendapatan besar bagi usahanya melalui software ini.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Tersedia Secara Mobile</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/BankingApplication.png' alt='Tampilan mock-up aplikasi bank' class='w-100'>
+            </figure>
+            <p>Perlu diketahui bahwa aplikasi untuk membantu kegiatan pengelolaan keuangan perusahaan tidak hanya tersedia dalam perangkat komputer saja. Melainkan perangkat lain seperti android dan juga iOS sudah bisa mengakses software semacam ini. Dimana cara pendapatan aplikasi ini pun terbilang cukup mudah, lantaran pengusaha hanya perlu menyelesaikan proses pengunduhan dan penginstalan di perangkat saja.</p>
+            <p>Untuk itu para pelaku usaha sudah tidak perlu membawa-bawa buku catatan ketika pergi kemana saja. Ditambah lagi sejumlah aplikasi mendukung untuk berbagai macam kebutuhan yang bersifat produktif. Diantaranya seperti melakukan pengiriman faktur, kemudian bisa juga untuk mengetahui berbagai informasi faktur, lalu juga data pengeluaran dan lain sebagainya.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Membantu Pengusaha Lebih Mudah Mengambil Keputusan</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/BusinessMeeting.png' alt='Ilustrasi rapat perusahaan' class='w-100'>
+            </figure>
+            <p>Berbagai kekhawatiran memang kadang kala menjadi masalah tersendiri bagi para pelaku usaha. Satu diantara beberapa daftar kekhawatiran tersebut adalah mengenai keuangan perusahaannya. Bahkan tidak sedikit pengusaha yang ragu jika dihadapkan pada sebuah situasi untuk mengambil keputusan. Terlebih lagi jika keputusan tersebut mempunyai resiko yang bisa dikatakan tinggi.</p>
+            <p>Namun dengan adanya software keuangan untuk perusahaan para pelaku usaha bisa mengetahui kondisi dari keuangan usahanya menggunakan cara yang lebih mudah. Selain gampang, software tersebut juga bisa bekerja dengan waktu yang relatif cepat. Hingga bisa membuat para pelaku usaha mampu mengetahui bagaimana pengelolaannya serta keputusan apa yang diambil pun bisa lebih diperhitungkan.</p>
+        </li>
+    </ol>
+    <p>Demikian pembahasan mengenai berbagai manfaat menggunakan aplikasi keuangan perusahaan untuk membantu mengelola usaha yang dijalankan. Ulasan tersebut kiranya cukup penting diketahui oleh para pelaku usaha yang merasa kesulitan ketika dihadapkan dengan beberapa hal mengenai sistem keuangan di perusahaannya. Dengan sejumlah beberapa manfaat di atas bisa menjadi pertimbangan bagi pelaku usaha untuk memakai software keuangan. <a href='<?= site_url() ?>'>Xunny.id</a> dapat membantu anda untuk mewujudkan keinginan anda untuk aplikasi keuangan perusahaan anda. Kontak kami <a href='<?= base_url() ?>/#Contact'>disini</a> untuk informasi lebih lanjut.</p>
+<?= $this->endSection() ?>
