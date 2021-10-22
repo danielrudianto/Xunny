cf5f1a83d07283b475e7e5aca8cf386fbe2a19d0
danielrudianto <45452945+danielrudianto@users.noreply.github.com>
2021-10-22 13:24:30 +0700

    22 Oktober 2021

---
+++ app/Views/blogs/13.php
@@ -0,0 +1,78 @@
+<?= $this->extend('/blog') ?>
+
+<?= $this->section('content') ?>
+    <h1 class='text-center'><?= $header['title'] ?></h1>
+    <img src='<?= base_url('assets/blog') . '/' . $header['id'] . '.webp' ?>' alt='<?= $header['title'] ?>' class='w-100' />
+    <h2 class='text-center h5'><?= $header['subtitle'] ?></h2>
+    <p class='text-center h5'><?= date("d M Y", strtotime($header['created_date'])) ?></p>
+    <hr>
+    <br>
+    <p>Dalam aktivitas berbisnis, kehadiran customer relationship management menjadi kebutuhan yang krusial. Hal ini dikarenakan CRM menyangkut pelanggan, yang merupakan aset berharga bagi sebuah perusahaan. Aplikasi customer relationship management hadir sebagai solusi untuk mempermudah perusahaan terhubung dengan pelanggannya. Hubungan baik dengan pelanggan dapat terjalin melalui komunikasi yang efektif, salah satunya melalui aplikasi.</p>
+    <p>Pelanggan merupakan sumber pendapatan dalam sebuah usaha, apapun bentuknya. Meski begitu, tidak semua bisnis memiliki akses komunikasi yang memadai untuk terhubung dengan pelanggannya secara berkelanjutan. Untuk itulah apps CRM ini dapat membantu menghasilkan pengalaman lebih baik pada customer. Inilah beberapa manfaat memakai aplikasi CRM:</p>
+    <ol>
+        <li>
+            <h2 class='h5 mt-5'>Mengelola Data Customer</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/MarketingSupport.png') ?>' alt='Ilustrasi marketing support' class='w-100'>
+            </figure>
+            <p>Manfaat utama dari aplikasi customer relationship management adalah sebagai pengelola data. Dalam perusahaan, mengumpulkan serta melakukan pengelolaan database customer cukup memakan waktu. Untuk itu, kehadiran aplikasi bisa membantu menyimpan serta mengelola database customer dengan lebih cermat dan aman. Berdasarkan data yang sudah mengelompok rapi, perusahaan lebih mudah untuk melakukan analisa.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Meningkatkan Kinerja</h2>
+            <p>Menggunakan software CRM berkualitas baik dapat membuat pekerjaan lebih efisien. Aplikasi CRM mampu melacak berbagai prospek dengan potensi menjanjikan sehingga bisa lebih menghemat waktu. Perusahaan bisa mencari riwayat pembelian dari aplikasi CRM sehingga mengetahui siapa saja pelanggan potensial.</p>
+            <p>Selanjutnya dapat melakukan komunikasi secara personal dengan pelanggan tersebut untuk meningkatkan kesempatan repeat order dan sejenisnya. Aplikasi ini juga dapat mencari tahu klien mana yang harus di follow up maupun chat serta email yang belum ditanggapi. Dengan demikian, aktivitas menjual secara umum bisa dikurangi dan menggunakan waktu yang tersisa untuk tugas-tugas lainnya.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Meningkatkan Keuntungan</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/Profit.png') ?>' alt='Ilustrasi meningkatkan keuntungan' class='w-100'>
+            </figure>
+            <p>Semua bidang usaha tentu ingin mendapatkan keuntungan, namun yang dimaksud di sini bukan hanya menaikkan jumlah penjualan saja. Aspek lain seperti operasional juga berperan penting. Sistem yang efisien dapat menurunkan biaya yang tidak perlu dan memaksimalkan dari apa yang sudah tersedia. Melalui aplikasi, karyawan bisa mengakses berbagai hal dengan cepat.</p>
+            <p>Karyawan dapat menjaga, bahkan meningkatkan engagement dari pelanggan. Misalnya dari chat serta telepon yang bisa dilacak kemudian diserahkan pada departemen tertentu untuk ditindaklanjuti. Dengan berkurangnya beban kerja yang terbantu oleh aplikasi, karyawan bisa bekerja lebih optimal untuk meningkatkan keuntungan secara bertahap.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Membantu Pengambilan Keputusan</h2>
+            <p>Aplikasi customer relationship management juga bisa digunakan dalam proses membuat keputusan. Sebelum memutuskan sesuatu, ada banyak hal yang perlu dilihat dan dipertimbangkan. Aplikasi CRM dapat memudahkan keputusan ini dengan memberikan data real time (sesuai yang terjadi saat ini).</p>
+            <p>Penyesuaian strategi bisa dilakukan dengan dasar data ini. Oleh karena itu penting memilih software handal, sehingga dapat menginformasikan berbagai hal yang penting diketahui. Data yang ditampilkan sangat dinamis dan mudah dilihat, dengan bantuan diagram dan keterangan lainnya tanpa harus membuat sendiri.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Menjaga Keamanan Data</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/Security.png') ?>' alt='Ilustrasi menjaga keamanan' class='w-100'>
+            </figure>
+            <p>Software CRM tentu dilengkapi dengan sistem keamanan yang bagus untuk menjaga data di dalamnya. Sistem CRM bersifat terpusat, sehingga lebih tertata dan aman daripada disimpan di banyak tempat yang berbeda. Perusahaan bisa berfokus pada berbagai hal sekaligus dalam platform CRM. Misalnya pembiayaan, infrastruktur, sumber daya, dan sebagainya.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Mengurangi Biaya Operasional yang Tidak Perlu</h2>
+            <p>Menekan biaya adalah lumrah dilakukan perusahaan untuk menjaga kondisi finansialnya tetap aman. Sayangnya, hal ini seringkali tidak diiringi pertimbangan matang sehingga berdampak pada menurunnya kinerja karyawan. Dengan aplikasi customer relationship management memang ada biaya yang dikeluarkan, namun hasilnya sepadan untuk jangka panjang.</p>
+            <p>Kepraktisan aplikasi ini dalam mengelola data dapat menghemat waktu serta energi. Jumlah biaya dapat ditekan dan dimanfaatkan untuk hal lain yang lebih efisien untuk memajukan perusahaan. Tidak perlu survey market, bingung mencari pangsa pasar dan sebagainya. Aplikasi CRM sudah mampu menjawab semua informasi tersebut.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Mempertahankan dan Memperbanyak Jumlah Konsumen</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/MaintainCustomer.png') ?>' alt='Ilustrasi mempertahankan konsumen' class='w-100'>
+            </figure>
+            <p>Mempertahankan konsumen bukan hal yang mudah. Aplikasi customer relationship management berperan dalam menghadirkan relasi jangka panjang dengan klien. Melalui CRM, perusahaan bisa mendapatkan pelanggan setia dengan mengidentifikasi kebutuhan apa yang sekiranya diinginkan klien. Relasi yang dipertahankan dengan baik dapat membuat konsumen merasa dihargai dan lebih betah.</p>
+            <p>CRM sudah dibekali dengan beberapa fitur andal untuk menjaga dan memperluas komunikasi dengan pelanggan. Dengan follow up yang praktis melalui aplikasi CRM, perusahaan bisa menggunakan data dari pelanggan untuk mengeluarkan produk yang lebih baik. Demikian juga dengan kualitas pelayanan yang diberikan. Perbaikan konsisten bisa mendatangkan lebih banyak klien kedepannya.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Mengoptimalkan Mobilitas di Lapangan & Sebagai GPS</h2>
+            <p>Teknologi membuat tim pekerja di lapangan lebih luwes dalam mengakses data. Mulai dari informasi seputar pembelian, kontak, dan jadwal penting lainnya. Saat di lapangan, data ini dapat diakses secara real time. Hal ini mempermudah untuk melakukan laporan atau memutuskan sesuatu sehingga dapat berlanjut ke mobilitas selanjutnya.
+            <p>Fitur yang terdapat dalam aplikasi CRM juga berfungsi ganda sebagai GPS. Keunggulan ini dapat digunakan untuk melacak seperti apa kinerja tim yang berada di lapangan secara live. Tanpa harus mendatangi langsung, manajer akan lebih mudah untuk melacak dan mengetahui progress dari proyek yang sedang berjalan. Meski di mode offline sekalipun, tim tetap bisa dilacak keberadaannya.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Tugas Harian Menjadi Otomatis</h2>
+            <figure>
+                <img src='<?= base_url('assets/blogContent/Automatic.png') ?>' alt='Ilustrasi automasi' class='w-100'>
+            </figure>
+            <p>Di perusahaan, sudah menjadi hal yang umum bahwa banyak tugas kecil yang perlu diselesaikan dengan cermat untuk mendapatkan hasil yang baik. Beberapa diantaranya seperti pengisian form, merancang perjanjian kontrak, membuat quotation, dan lain-lain. Pekerjaan ini cukup menghabiskan waktu, namun memiliki peran yang krusial bagi perusahaan.</p>
+            <p>Untuk itulah memanfaatkan aplikasi customer relationship management bisa menjadi pilihan tepat. Tugas-tugas seperti pengisian form dan sebagainya bisa dialihkan menjadi otomatis. Ada fitur bernama sales force automation yang dapat digunakan untuk mengerjakan hal-hal seperti tadi. Perusahaan bisa berfokus pada close deal sedangkan sisanya akan dikelola secara mendetail oleh aplikasi.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Pembuatan Strategi Marketing</h2>
+            <p>Bukan hanya menyediakan data dari pelanggan, fitur yang dimiliki CRM juga memungkinkan perusahaan untuk melihat kondisi pasar. Situasi bisa saja berubah dengan cepat, padahal tidak semua orang bisa tahu berita terbaru. CRM menjawab kebutuhan ini dengan menghadirkan data pasar. Data ini dapat dijadikan patokan untuk menyusun strategi.</p>
+            <p>Membuat strategi memerlukan data yang banyak sehingga bisa menjadi bahan analisis. Untuk menghasilkan produk maupun jasa yang tepat sasaran, pemahaman mengenai situasi pasar dan konsumen sangat dibutuhkan. Kebutuhan ini bisa dicapai dengan software CRM berkualitas baik. Investasi untuk aplikasi sejenis ini sangat berguna untuk waktu lama sehingga tidak akan rugi untuk memakainya.</p>
+        </li>
+    </ol>
+    <p>Itulah sederet manfaat menggunakan aplikasi customer relationship management bagi perusahaan. Kehadiran aplikasi ini sudah sepatutnya difokuskan oleh bisnis masa kini, terutama dengan pesatnya kemajuan internet. Menghargai pelanggan juga termasuk upaya yang dapat meningkatkan kemajuan bisnis. Dengan komunikasi yang terjalin secara menyenangkan, peluang mendapatkan pelanggan setia (loyal) akan semakin bertambah. <a href='<?= site_url() ?>'>Xunny.id</a> dapat membantu anda untuk mewujudkan keinginan anda untuk membuat aplikasi Customer Relation Management ( CRM ). Kontak kami <a href='<?= base_url() ?>/#Contact'>disini</a></p>
+<?= $this->endSection() ?>
