b49a54f4eb33da83c717637df1468ff13cd48e41
Daniel Tri <danielrudianto12@gmail.com>
2021-11-14 22:36:06 +0700

    14 November 2021

--- app/Views/blogs/28.php
+++ app/Views/blogs/28.php
@@ -5,5 +5,83 @@
     <p>Dari sisi biaya, memang promosi lewat media digital jauh lebih murah. Selain itu, jangkauan target juga bisa jadi lebih tinggi. Apalagi, ada ratusan juta orang yang menggunakan dunia maya.</p>
     <p>Oleh sebab itu, Anda perlu mengetahui cara apa saja yang paling efektif untuk promosi secara online. Namun, perlu diperhatikan bahwa ada beberapa hal yang perlu dipahami terlebih dahulu.</p>
     <p>Sebenarnya, ada banyak sekali cara yang bisa dipilih. Namun, setidaknya 7 cara ini bisa dijadikan rekomendasi. Lalu, cara apa saja yang bisa digunakan secara efektif?</p>
-    <p>Setelah mengetahui pentingnya artikel dalam website anda, anda dapat mencoba dan merasakan sendiri manfaatnya secara langsung. <a href='<?= base_url() ?>'>Xunny.id</a> dapat menyediakan jasa untuk melakukan penulisan artikel untuk website anda. Hubungi kami <a href='<?= base_url() ?>/#Contact'>disini</a> untuk melakukan konsultasi.</p>
+    <ol>
+        <li>
+            <h2 class='h5 mt-5'>Ketahui Target Konsumen</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/Target.png' class='w-100' alt='Ilustrasi menargetkan konsumen'>
+            </figure>
+            <p>Perlu dipahami bahwa promosi yang baik adalah sesuai sasaran. Setiap target pasti memiliki metode dan konten marketing yang berbeda. Itulah mengapa penting sekali memperhatikan target pasar sebelum membuat promosi.</p>
+            <p>Cara promosi online yang menarik bisa Anda mulai dengan melakukan riset terlebih dahulu. Misalnya, jika produk Anda adalah untuk generasi milenial, maka konten promosi juga harus dibuat kreatif, bukan?</p>
+            <p>Anda bisa memanfaatkan model promosi viral untuk menarik perhatian. Perlu diketahui bahwa saat ini banyak orang lebih suka dengan sesuatu yang viral. Bahkan, impresi dari hal tersebut terus meningkat.</p>
+            <p>Untuk bentuk promosinya Anda bisa menyesuaikan dengan keadaan. Anda bisa menggunakan konten video, foto, atau sejenisnya. Hal paling penting adalah bagaimana Anda mampu mengetahui target konsumen tersebut.</p>
+        </li>
+
+        <li>
+            <h2 class='h5 mt-5'>Kerjasama dengan Influencer</h2>
+            <p>Cara promosi online yang menarik selanjutnya adalah menjalin kerjasama dengan public figure. Memang, biaya yang akan dihabiskan tidak sedikit. Namun, hasil yang didapatkan tentu sesuai.</p>
+            <p>Namun, tentu Anda harus memilih influencer yang memiliki banyak pengikut. Selain itu, memilih public figure dengan budget terjangkau adalah hal penting. Anda bisa melakukan riset untuk hal ini.</p>
+            <p>Biasanya, setelah menjalin kerjasama, angka penjualan bisa meningkat. Belum lagi jika public figure tersebut memiliki track record baik. Masyarakat juga pastinya akan percaya dengan produk tersebut.</p>
+            <p>Biasanya, setelah menjalin kerjasama, angka penjualan bisa meningkat. Belum lagi jika public figure tersebut memiliki track record baik. Masyarakat juga pastinya akan percaya dengan produk tersebut.</p>
+        </li>
+
+        <li>
+            <h2 class='h5 mt-5'>Gunakan Hashtag</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/Hashtag.png' class='w-100' alt='Ilustrasi penggunaan hashtag pada media sosial'>
+            </figure>
+            <p>Jika Anda lebih memilih untuk mempromosikan sebuah produk lewat social media Instagram, maka jangan lupa gunakan hashtag. Ini merupakan salah satu kata kunci untuk memudahkan orang lain menemukan sebuah produk.</p>
+            <p>Misalnya, jika Anda menjual produk pakaian, maka bisa menggunakan hashtag pakaianmurah, pakaiankeren, dsb. Perlu Anda ingat, penggunaan hashtag tidak memerlukan spasi. Jadi, harus menggabungkan beberapa kata jadi satu.</p>
+            <p>Cara promosi online yang menarik ini sebenarnya sangat efektif. Hanya saja, algoritma hashtag di social media Instagram terus berubah setiap saat. Itulah mengapa perlu melakukan riset pada periode waktu tertentu.</p>
+            <p>Selain itu, ada aturan di mana Anda bisa menggunakan hastag. Tidak masalah menggunakan banyak hastag. Namun, gunakan hashtag yang relevan sehingga tidak dianggap sebagai spam oleh system.</p>
+        </li>
+
+        <li>
+            <h2 class='h5 mt-5'>Berikan Feedback yang Responsif</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/Feedback.png' class='w-100' alt='Ilustrasi pemberian feedback'>
+            </figure>
+            <p>Sebaik apapun konten promosi yang Anda lakukan, semuanya harus diimbangi dengan feedback. Maksudnya adalah Anda harus mampu memberikan respons cepat setiap kali ada tanggapan dari calon customer.</p>
+            <p>Banyak kasus di mana customer merasa diabaikan karena tidak menerima tanggapan. Hal ini bisa saja membuat customer merasa enggan untuk melanjutkan pembelian. Bahkan, bisa saja mereka pindah produk.</p>
+            <p>Semakin baik respon Anda berikan, maka hasilnya juga semakin baik. Ada penelitian bahwa bisnis yang memberikan respon kepada konsumen memiliki kemungkinan lebih besar untuk mencapai angka penjualan tertentu.</p>
+            <p>Cara promosi online yang menarik ini mungkin masih belum diterapkan oleh pemilik usaha. Namun, Anda perlu melakukan ini supaya produk yang ditawarkan bisa sampai ke tangan customer lebih cepat.</p>
+        </li>
+
+        <li>
+            <h2 class='h5 mt-5'>Adakan Challenge</h2>
+            <p>Untuk membangun sebuah brand awareness, ada banyak metode bisa dilakukan. Challenge merupakan salah satu cara yang bisa dipakai. Apalagi, challenge sudah banyak dimanfaatkan oleh brand besar.</p>
+            <p>Berkembangnya teknologi juga membuat metode ini semakin popular. Munculnya berbagai platform berbagi video membuat challenge semakin mudah dilakukan. Manfaatkan kesempatan ini untuk mempromosikan produk Anda kepada calon customer.</p>
+            <p>Cara promosi online yang menarik ini bisa dilakukan dengan cara memberikan syarat untuk mengunggah video saat menggunakan produk anda. Dengan cara ini, Anda akan mendapatkan banyak keuntungan sekaligus.</p>
+            <p>Selain membangun brand awareness, produk tersebut juga akan dipromosikan oleh peserta challenge secara tidak sadar. Tentu hal ini akan membuat produk tersebut semakin dikenal, bukan?</p>
+        </li>
+
+        <li>
+            <h2 class='h5 mt-5'>Bangun Komunikasi yang Baik</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/Communication.png' class='w-100' alt='Ilustrasi melakukan komunikasi'>
+            </figure>
+            <p>Bukan rahasia umum lagi jika membangun sebuah komunikasi dengan customer adalah media promosi paling baik. Bahkan, bukan tidak mungkin mereka akan membeli kembali produk tersebut.</p>
+            <p>Namun, bagaimana cara membangun sebuah komunikasi yang baik? Jika diperhatikan, ada banyak sekali cara bisa Anda lakukan. Salah satu contohnya adalah dengan sering mengunggah konten terbaru.</p>
+            <p>Misalnya, jika ada customer mengunggah foto saat menggunakan produk anda, maka bisa melakukan repost unggahan tersebut. Ini adalah bentuk penghargaan kepada customer yang telah menggunakan produk tersebut.</p>
+            <p>Brand besar selalu menerapkan metode seperti ini. Bahkan, jika perlu Anda bisa menanggapi komentar maupun pesan langsung dengan humor. Ini bisa membuat kedekatan secara emosional dengan customer.</p>
+        </li>
+
+        <li>
+            <h2 class='h5 mt-5'>Gunakan Promo Menarik</h2>
+            <p>Cara promosi online yang menarik selanjutnya adalah sering mengadakan promo menarik. Tidak ada orang yang tidak menyukai promo. Bahkan, beberapa orang rela melakukan apa saja demi mendapatkan hal tersebut.</p>
+            <p>Tentu, promo bisa berbentuk banyak hal. Diskon, giveaway, hingga bebas biaya kirim adalah sebagian dari contoh promo. Anda bisa menyesuaikan hal tersebut untuk menarik lebih banyak calon customer.</p>
+            <p>Pada dasarnya, cara ini memungkinkan Anda untuk tidak mendapatkan untung besar di awal. Namun, dengan berjalannya waktu, keuntungan lebih besar bisa didapatkan ketika banyak customer telah memiliki brand awareness.</p>
+        </li>
+
+        <li>
+            <h2 class='h5 mt-5'>Manfaatkan Berbagai Content Marketing</h2>
+            <figure>
+                <img src='<?= base_url() ?>/assets/blogContent/ContentMarketing.png' class='w-100' alt='Ilustrasi konten marketing'>
+            </figure>
+            <p>Cara terakhir adalah dengan menggunakan semua jenis content marketing. Ada banyak sekali content marketing mulai dari foto, video, hingga artikel. Gunakan salah satu atau semua sekaligus jika diperlukan.</p>
+            <p>Jika Anda merasa kesulitan untuk membuat sebuah konten menarik, maka bisa menggunakan agensi. Banyak sekali agensi yang telah menawarkan konten menarik. Bahkan, semua ide konten bisa dikembangkan dengan mudah.</p>
+            <p>Namun, Anda harus benar-benar memilih agensi tersebut dengan benar. Setidaknya, pilih agensi dengan track record pasti. Dengan begitu, Anda bisa mendapatkan konten yang sesuai dan pastinya efektif.</p>
+        </li>
+    </ol>
+    <p>Beriklan secara fisik merupakan metode yang sangat penting, seperti <a rel='dofollow' href='https://wesignmedia.com'>melakukan pemasangan plang iklan, billboard, ataupun banner</a>, namun dengan adanya era <i>New normal</i>, melakukan pemasaran secara online pun menjadi krusial bagi seluruh perusahaan. Kami harap artikel kami dapat membantu kalian untuk melakukan pemasaran secara digital.</p>
 <?php $this->endSection(); ?>
