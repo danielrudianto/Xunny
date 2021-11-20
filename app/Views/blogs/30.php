4f485991e164628a9a13052562ab0e9e66c81539
danielrudianto <45452945+danielrudianto@users.noreply.github.com>
2021-11-21 01:58:02 +0700

    Upddate blog

--- app/Views/blogs/30.php
+++ app/Views/blogs/30.php
@@ -9,6 +9,71 @@
     <p>Mungkin beberapa software sudah dikenal oleh masyarakat. Beberapa bahkan menjadi lebih terkenal dalam beberapa tahun terakhir. Berikut beberapa daftar aplikasi yang bisa dipakai untuk online meeting baik gratis atau berbayar.</p>
 
     <p>Ada banyak sekali aplikasi yang bisa digunakan. Beberapa bisa digunakan secara gratis, sedangkan sebagian lain bisa dipakai dengan cara membayar. Berikut ini ada beberapa daftar aplikasi yang populer.</p>
+    <ol>
+        <li>
+            <h2 class='h5 mt-5'>Whatsapp</h2>
+            <p>Whatsapp merupakan salah satu aplikasi yang sudah sangat popular. Semua smartphone baru pasti sudah terpasang aplikasi ini dari awal. Jadi, bisa langsung digunakan tanpa perlu melakukan pengunduhan.</p>
+            <p>Beberapa prang mungkin lebih sering menggunakan Whatsapp untuk berkirim pesan singkat atau video call. Tapi, Whatsapp juga bisa dipakai untuk aktivitas meeting online, meskipun hanya terbatas untuk beberapa orang.</p>
+            <p>Ya, kapasitas untuk melakukan online meeting hanya 8 orang. Namun, tidak masalah jika memang tidak mengharuskan untuk dihadiri oleh banyak orang. Apalagi, Anda juga tidak membutuhkan biaya tambahan.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Zoom</h2>
+            <p>Jika berbicara tentang aplikasi meeting online, Zoom sudah pasti akan selalu disebutkan. Sejak booming beberapa tahun terakhir, Zoom terus menjadi salah satu aplikasi yang paling banyak digunakan.</p>
+            <p>Perlu diketahui bahwa Zoom menyediakan layanan gratis serta berbayar. Layanan gratis ini hanya bisa dilakukan dengan syarat tertentu. Anda bisa menggunakannya dengan maksimal 2 orang untuk waktu tidak terbatas.</p>
+            <p>Jika diikuti oleh banyak orang, maka Zoom menyediakan layanan gratis untuk beberapa menit. Namun, jika ingin digunakan untuk kepentingan banyak orang dalam waktu lama, maka bisa berlangganan terlebih dahulu.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Google Meet</h2>
+            <p>Selain Zoom, ada juga aplikasi meeting online yang cukup popular, Google Meet. Software ini merupakan layanan yang diberikan oleh Google. Menariknya, Google Meet bisa digunakan tanpa perlu membayar terlebih dahulu.</p>
+            <p>Dari sisi kemudahan, Google Meet dinilai lebih mudah diaplikasikan. Apalagi, fitur yang telah disediakan juga lebih friendly. Banyak online meeting atau conference menggunakan Google Meet karena lebih mudah.</p>
+            <p>Kelebihan lain dari software ini adalah Anda tidak perlu menginstall aplikasinya. Layanan ini bahkan bisa digunakan hanya dengan menggunakan website. Artinya, ini bisa mengurangi beban memori pada perangkat mobile.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Skype</h2>
+            <p>Mungkin Anda sudah lama mendengar software satu ini. Ya, Skype merupakan salah satu perangkat lunak panggilan video yang sudah ada sejak lama. Bahkan, skype sudah hadir sebelum populernya Zoom.</p>
+            <p>Skype merupakan perangkat lunak hybrid yang bisa digunakan di PC maupun perangkat mobile. Biasanya, skype dipakai untuk online meeting seperti kelas online hingga pertemuan lainnya.</p>
+            <p>Menariknya, Skype bisa digunakan hingga 25 orang sekaligus. Anda yang membutuhkan software dengan daya tamping besar serta mudah dipakai, maka Skype bisa Anda jadikan sebagai alternatif pilihan.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Slack</h2>
+            <p>Jika ingin menggunakan aplikasi meeting online yang berbeda, maka Slack bisa Anda coba. Mungkin masih banyak orang belum mengenal Slack. Namun, aplikasi ini banyak dipakai untuk aktivitas tertentu.</p>
+            <p>Biasanya, aktivitas pelatihan online yang berkelanjutan sering menggunakan Slack. Apalagi, Slack juga menyediakan fitur lain seperti share materi hingga dokumen. Tampilan interface juga jauh lebih menarik dari pada aplikasi sejenis.</p>
+            <p>Anda bisa mengunduh Slack di perangkat mobile. Jangan khawatir, Slack mampu menampung hingga 15 orang sekaligus. Jadi, cukup sesuai dengan jumlah yang tidak terlalu banyak atau sedikit.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Cisco Webex</h2>
+            <p>Mungkin, masih banyak orang yang belum mendengar nama Cisco Webex. Namun, banyak perusahaan besar dunia justru memanfaatkan Cisco Webex saat ingin melakukan aktivitas online meeting.</p>
+            <p>Perlu diketahui bahwa Cisco Webex menjadi salah satu aplikasi meeting online dengan menyediakan fitur professional. Layanan yang diberikan juga dinilai lebih baik dari pada aplikasi sejenis lainnya.</p>
+            <p>Dengan banyaknya fitur professional tersebut, Cisco Webex bisa jadi pilihan utama jika ingin mengadakan online meeting. Tentunya, untuk menggunakannya Anda harus mengikuti aturan yang telah ditetapkan sebelumnya.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Jitsi Meet</h2>
+            <p>Jitsi Meet mungkin juga masih belum banyak dikenal oleh masyarakat. Memang, aplikasi ini tidak popular layaknya Zoom maupun Google Meet. Namun, dari sisi kegunaan, Jitsi Meet tidak kalah dengan lainnya.</p>
+            <p>Selain bisa menggunakan lewat website resminya, Anda juga dapat mengunduh aplikasinya. Tentu fiturnya tidak akan berbeda jauh. Anda akan merasakan pengalaman yang berbeda saat menggunakan Jitsi Meet.</p>
+            <p>Di sisi lain, Jitsi Meet sendiri telah diunduh lebih dari 10 juta kali di Google Playstore. Tentu hal ini menunjukkan bahwa Jitsi Meet memiliki beberapa kelebihan tersendiri.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Go To Meeting</h2>
+            <p>Aplikasi meeting online lainnya yang bisa dijadikan sebagai rekomendasi adalah Go To Meeting. Sama seperti aplikasi lain, Anda bisa menggunakan Go To Meeting untuk mengadakan pertemuan secara virtual.</p>
+            <p>Dilihat dari beberapa fiturnya, aplikasi ini sebenarnya tidak memiliki perbedaan jauh dengan software sejenis. Namun, beberapa ada batasan karena dinilai tidak lengkap layaknya Zoom maupun Google Meet.</p>
+            <p>Namun, jika ingin menggunakan perangkat yang lebih mudah dan sederhana, maka aplikasi ini bisa jadi pilihan terbaik. Jumlah partisipan dalam sekali panggilan juga tergolong banyak.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Zoho Meeting</h2>
+            <p>Meskipun kurang popular, Zoho Meeting telah di unduh lebih dari 100 ribu orang. Ada banyak sekali fitur pada aplikasi ini seperti layanan berbagai pilihan layar.</p>
+            <p>Menariknya, software ini juga menyediakan fitur polling serta QnA. Fitur ini jelas sangat berguna karena bisa dipakai untuk melihat atau menentukan sebuah keputusan setelah adanya pertemuan online. </p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Bluejeans Video Conferencing</h2>
+            <p>Bluejeans Video Conferencing menjadi salah satu aplikasi meeting online yang cukup menarik. Memang, Bluejenas masih belum cukup popular. Hal ini karena promosinya masih terbatas untuk beberapa perusahaan saja.</p>
+            <p>Salah satu fitur dari Bluejeans Video Conferencing adalah menyediakan slot untuk partisipan. Dalam sekali waktu, Anda bisa mengundang sekitar 150 partisipan. Jadi, sudah pasti akan sangat berguna.</p>
+        </li>
+        <li>
+            <h2 class='h5 mt-5'>Online Meeting Webinars</h2>
+            <p>Rekomendasi aplikasi terakhir adalah Online Meeting Webinars. Seperti namanya, aplikasi ini menyediakan layanan khusus untuk aktivitas webinar atau sejenisnya. Layanan ini sekarang sangat popular di tengah berkembangnya dunia teknologi.</p>
+            <p>Perlu Anda pahami bahwa layanan ini memberikan kemudahan bagi siapa saja. Salah satunya adalah bisa membagikan rapat melalui link ke berbagai social media seperti Instagram.</p>
+        </li>
+    </ol>
 
-    <p>Customer service merupakan salah satu pilar penting dalam seluruh perusahaan, baik barang, jasa, ataupun manufaktur. Kami harap artikel ini dapat membantu anda untuk dapat meutilisasi aplikasi aplikasi ini guna meningkatkan kualitas customer service kalian. Karena dengan semakin meningkatnya <a href='<?= base_url() ?>/Blogs/Blogs/inilah-manfaat-website-bagi-perusahaan-tak-perlu-ragu-untuk-membuatnya'>branding</a>, semakin tinggi juga kebutuhan untuk customer service. </p>
+    <p>Demikian pembahasan kami mengenai aplikasi online meeting dengan fitur terlengkap. Kami harap pembahasan kami dapat membantu anda untuk memiliki alternatif di luar dari pilihan yang saat ini sudah anda sukai.</p>
 <?php $this->endSection(); ?>
---
+++ app/Views/blogs/31.php
@@ -0,0 +1,39 @@
+<?php $this->extend('/blog'); ?>
+<?php $this->section('content'); ?>
+    <p>Apa manfaat website dalam bisnis online tentunya waji dipahami oleh semiua pengusaha. Apalagi perkembangan teknologi informasi yang semakin luas tentunya menjadi salah satu faktor mengapa perlu pengembangan yang harus dilakukan, misalnya dalam bentuk website.</p>
+    <p>Sebagai media atau platform dengan basis online, kebutuhan website tentunya menjadi semakin kompleks saat ini. Pertimbangan untuk menggunakan website saat ini tidak hanya sebatas untuk memberikan informasi secara menyeluruh dan tentunya lebih spesifik.</p>
+    <p>Dahulu website mungkin hanya dijadikan media untuk sekedar membaca informasi yang sedang terjadi. Akan tetapi pertumbuhan pola pasar serta pola belanja masyarakat secara tidak langsung mengubah paradigma website menjadi lebih baik dan tentunya lebih kompleks.</p>
+    <p>Hal inilah yang secara tidak langsung bisa membuat kapasitas pola penggunaan website saat ini hadir untuk kebutuhan bisnis secara online. Lantas Apa manfaat website dalam bisnis online yang bisa memberikan dampak cukup signifikan dalam dunia bisnis.</p>
+    <p>Sebelum masuk pada pembahasna tersebut maka ada baiknya juga untuk Anda ketahui bahwa saat perkembangan bisnis online juga tidak kalah saing. Berbeda dengan beberapa periode sebelumnya, saat ini kecenderungan masyarakat untuk berbelanja telah berubah.</p>
+    <p>Hal ini bisa dilihat dari banyaknya platform markatpolace yang telah mampu mewadahi semua kebutuhan belanja masyarakat. Perubahan pola belanja inilah yang kemudian menarik perhatian dari beberapa pengusaha dalam memasarkan produknya.</pl>
+    <p>Sebagai catatan, selain website ada banyak lagi cara media lain yang bisa digunakan untuk promosi online. Dan tentunya media tersebut memiliki berbagai macam kelebihan dan kekurangan masing-masing.</p>
+    <p>Lantas Apa manfaat website dalam bisnis online, berikut ini adalah penjelasannya.</p>
+
+    <h2 class='h5 mt-5'>Untuk Peningkatan Kredibilitas Suatu Bisnis</h2>
+    <p>Aspek pertama yang penting untuk diperhatikan adalah bahwa keberadaan website memang sangat mempunyai andil untuk meningkatkan kredibilitas suatu bisnis online. Apalagi saat ini pertimbangan untuk menjadi lebih dikenal menjadi salah satu aspek penting.</p>
+    <p>Salah satu aspek penting berupa pengenalan produk ini tentunya dengan menggunakan website. Apalagi saat ini penggunaan website sebagai salah satu media pemasaran menjadi lebih mudah untuk dikenali oleh semua public melalui intensitas menggunakan internet.</p>
+    <p>Pada aspek lainnya pertimbangan untuk meningkatkan kredibilitas bisa dibangun dengan melakukan modifikasi website. Salah satu bentuk modifikasi tersebut ialah dengan menambahkan gravis yang cenderung bagus dan tentunya menarik perhatian pengunjung.</p>
+    <p>Semakin sering website tersebut mendapatkan pengunjung maka secara tidak langsung akan mempengaruhi pada kredibilitas website itu sendiri. Hasilnya pola penjualan akan lebih meningkat dari waktu ke waktu dan tentunya mampu memenuhi target penjualan bisnis.</p>
+    <p>Hingga saat ini perkembangan beberapa pengusaha yang hendak memperbaiki websitenya telah melakukan berbagai macam car. Salah satunya dengan memakai beberapa jasa untuk membuat website tersebut bisa menjadi lebih menarik.</p>
+
+    <h2 class='h5 mt-5'>Memudahkan Mendapatkan Suatu Informasi</h2>
+    <p>Pertimbangan selanjutnya yang cukup penting untuk diperhatikan adalah bahwa kapasitas pola pencarian informasi menjadi lebih mudah dengan penggunaan website. Lalu Apa manfaat website dalam bisnis online yang bisa digunakan untuk memudahkan pencarian. Berikut ini adalah sistem yang biasa digunakan.</p>
+    <ol>
+        <li>
+            <p>Sistem informasi menjadi lebih mudah untuk diketahui jika pemiliki website mampu memberikan pengaturan berupa SEO. Seperti yang telah diketahui sebelumnya bahwa kapasitas SEO sangat menunjang optimasi website tersebut bisa diakses pengguna.</p>
+            <p>Sistem informasi menjadi lebih mudah untuk diketahui jika pemiliki website mampu memberikan pengaturan berupa SEO. Seperti yang telah diketahui sebelumnya bahwa kapasitas SEO sangat menunjang optimasi website tersebut bisa diakses pengguna.</p>
+        </li>
+        <li>
+            <p>Selain adanya SEO pertimbangan lain yang secara praktik mampu memberikan kemudahan dalam mendapatkan informasi adalah kemudahan website untuk diakses. Hal tersebut tentu berlaku pada smartphone yang bisa diakses dengan mudah.</p>
+            <p>Dengan adanya smartphone maka secara tidak langsung penting bagi beberapa pihak untuk lebih mudah mengakses informasi secara menyeluruh. Hal ini juga yang menjamin kapasitas bisnis Anda lebih mudah untuk ditemukan secara lebih fleksibel.</p>
+        </li>
+    </ol>
+    <p>Semua aspek diatas tentu bisa dilakukan secara bersamaan, sehingga mampu memberikan manfaat berupa dampak pengenalan produk menjadi lebih baik. Inilah yang menjadi dasar utama pentingnya penggunaan website.</p>
+
+    <h2 class='h5 mt-5'>Penghematan Waktu dan Biaya</h2>
+    <p>Aspek selanjutnya yang penting untuk diperhatikan adalah bahwa dengan adanya penggunaan website maka akan ada pemangkasan cukup besar. lantas Apa manfaat website dalam bisnis online sehingga bisa memangkas biaya dan waktu tersebut.</p>
+    <p>Hal pertama yang penting dipahami bahwa kapasitas pola penggunaan website secara tidak langsung lebih menghemat waktu untuk media pemasaran. Pasalnya dengan media website para pengusaha tidak perlu susah untuk melakukan promosi di lapangan.</p>
+    <p>Promosi dilapangan juga sangat cenderung membutuhkan biaya cukup banyak, mulai dari brosur hingga biaya transportasi. Akan tetapi dengan penggunaan website maka secara tidak langsung semua biaya tersebut akan terpangkas secara otomatis dan tentu lebih hemat.</p>
+
+    <p><a href='<?= base_url() ?>'>Xunny.id</a> dapat membantu anda untuk membangun website untuk perusahaan anda. Anda dapat melihat <a href='<?= base_url() ?>/#Portfolio'>portfolio</a> kami tentang pekerjaan kami sebelumnya. <a href='<?= base_url() ?>/Blogs/inilah-manfaat-website-bagi-perusahaan-tak-perlu-ragu-untuk-membuatnya'>Pembuatan website bagi perusahaan</a> tentu akan menjawab Apa manfaat website dalam bisnis online dengan hasil yang cukup mempuni dalam mengembangkan bisnis Anda.</p>
+<?php $this->endSection(); ?>
