15d11874392cccb4cc0b215799ae28449f7d0885
danielrudianto <45452945+danielrudianto@users.noreply.github.com>
2021-11-23 17:05:39 +0700

    Update database

--- db/xunny.sql
+++ db/xunny.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2021 at 10:41 AM
+-- Generation Time: Nov 23, 2021 at 11:05 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
