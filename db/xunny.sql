5d479ec404f4649bad9fde0449f86145c7f6500b
danielrudianto <45452945+danielrudianto@users.noreply.github.com>
2021-12-04 10:05:02 +0700

    Update Database

--- db/xunny.sql
+++ db/xunny.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2021 at 11:05 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- Generation Time: Dec 04, 2021 at 04:03 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -65,18 +64,20 @@ INSERT INTO `blog` (`id`, `title`, `subtitle`, `words`, `created_by`, `created_d
 (20, 'Fungsi Aplikasi Pengolah Angka yang Menguntungkan Bagi Perusahaan', 'Dengan adanya teknologi yang membantu kita untuk melakukan kalkulasi kita tidak lagi perlu melakukan kalkulasi secara manual secara umum. Perusahaan dengan budaya untuk melakukan perhitungan secara manual perlahan mulai beralih ke sistem yang lebih canggih untuk memperoleh hasil yang sama atau lebih dengan sumber daya lebih rendah.', 1030, 'Daniel Tri', '2021-10-23', 'Pengola angka, aplikasi perusahaan, rumus, perhitungan skala besar, grafik, data, cepat, meminimalisir kesalahan, hemat biaya'),
 (21, 'Mengenal Aplikasi Penjualan dan Stok Barang dan Urgensinya', 'Pencatatan stok dan penjualan merupakan isu yang menantang dan umum bagi hampir seluruh pelaku usaha yang bergerak di bidang perdagangan. Pada artikel ini, kita akan membahas sebagaimana penting aplikasi tersebut dan bagaimana implementasinya.', 1000, 'Daniel Tri', '2021-10-23', 'Stok, penjualan, urgensi, pendataaan, harga jual, neraca keuangan, kondisi persediaan'),
 (22, 'Ketahui Apa Saja Manfaat Digital Marketing dalam Ulasan Berikut Ini', 'Dunia digital tidak lepas dari istilah digital marketing, sebuah metode marketing baru yang ditawarkan oleh teknologi untuk dapat menjangkau bahkan lebih banyak konsumen sekaligus. Apa saja manfaat melakukan digital marketing hari ini? Mari simak di artikel ini', 1200, 'Daniel Tri', '2021-10-24', 'Digital marketing, hemat biaya, komunikasi, pasar, jangkauan pasar, kompetisi bisnis, real-time, profesional'),
-(23, 'Secure Shell dan manfaatnya bagi keamanan informasi', 'Semakin berkembang teknologi, semakin berkembang pula ilmu meretas atau hacking. Untuk itu kita memerlukan keamanan untuk informasi terutama untuk data yang disimpan pada server. Salah satu cara untuk berkomunikasi dengan server yang aman adalah menggunakan Secure Shell atau seringkali disebut SSH. Mari kita bahas pada artikel ini tentang SSH.', 845, 'Daniel Tri', '2021-11-01', 'Enkripsi, SSH, Secure Shell, Encryption, Asymmetric Encryption, Hashing, Symmetrical encryption, Keamanan, Server, Client'),
-(24, 'Benarkah Media Promosi Online Paling Efektif Bisa Meningkatkan Penjualan', 'Media promosi online paling efektif merupakan cara yang dilakukan atau dipakai seorang pebisnis untuk meningkatkan pendapatan. Berbagai bentuk aktivitasnya seperti menginformasikan, membagikan, dan menawarkan produk atau jasa di perusahaan. Hal tersebut dilakukan untuk menarik perhatian public terhadap barang atau jasa yang ditawarkan. Dalam artikel ini, kita akan membahas mengenai media promosi paling efektif untuk meningkatkan penjualan', 1054, 'Daniel Tri', '2021-10-25', 'Promosi online, meningkatkan penjualan, efektif, menawarkan produk, menarik perhatian, brand awareness, loyalitas, jangkauan'),
-(25, 'OS Linux Terbaik 2021 Penggunaan Sehari hari', 'Operating system Linux merupakan OS yang bersifat open-source dan gratis. Sebagian orang mencoba menghindari Linux dikarenakan ketenarannya terhadap kesulitan pemakaian. Namun pada 2021 ini, Linux telah berubah dan menyajikan beragam pilihan OS yang lebih user friendly. Mari kita simak OS Linux terbaik pada tahun 2021 untuk penggunaan sehari hari', 444, 'Daniel Tri', '2021-11-06', 'Linux, OS Linux\r\n'),
-(26, 'Kelebihan dan Kekurangan Linux yang Anda Perlu Ketahui', 'Operating System Linux telah hadir sejak awal 1990 dan hingga saat ini, OS Linux sangat umum ditemukan di server dikarenakan keamanan dan performanya. Mari kita bahas apa saja kekurangan dan kelebihdan dari OS ini?', 457, 'Daniel Tri', '2021-11-04', 'OS Linux, Performa, OS Gratis, Kelebihan Linux, Kekurangan Linux'),
+(23, 'Mengenal Pentingnya Aplikasi Antrian Berbasis Web yang Memudahkan Pekerjaan', 'Aplikasi antrian berbasis web bisa dibilang merupakan suatu aplikasi yang dirancang oleh suatu pihak untuk mempermudah kegiatan dalam masyarakat. Kegiatan ini biasanya akan menyangkut dalam kegiatan yang mengarah dalam keramaian. Hal inilah yang kemudian membuat suatu lembaga atau perusahaan memberlakukan sistem antrian. Menjaga ketertiban dan memberikan pelayanan lebih maksimal tentu adalah alasannya.', 1100, 'Daniel Tri', '2021-10-25', 'antrian online, aplikasi antrian, fitur suara, data penyimpanan'),
+(24, 'Benarkah Media Promosi Online Paling Efektif Bisa Meningkatkan Penjualan', 'Media promosi online paling efektif merupakan cara yang dilakukan atau dipakai seorang pebisnis untuk meningkatkan pendapatan. Berbagai bentuk aktivitasnya seperti menginformasikan, membagikan, dan menawarkan produk atau jasa di perusahaan. Hal tersebut dilakukan untuk menarik perhatian public terhadap barang atau jasa yang ditawarkan. Dalam artikel ini, kita akan membahas mengenai media promosi paling efektif untuk meningkatkan penjualan', 1100, 'Daniel Tri', '2021-10-25', 'Promosi online, meningkatkan penjualan, efektif, menawarkan produk, menarik perhatian, brand awareness, loyalitas, jangkauan'),
+(25, 'OS Linux Terbaik 2021 Penggunaan Sehari hari', 'Operating system Linux merupakan OS yang bersifat open-source dan gratis. Sebagian orang mencoba menghindari Linux dikarenakan ketenarannya terhadap kesulitan pemakaian. Namun pada 2021 ini, Linux telah berubah dan menyajikan beragam pilihan OS yang lebih user friendly. Mari kita simak OS Linux terbaik pada tahun 2021 untuk penggunaan sehari hari', 765, 'Daniel Tri', '2021-11-06', 'Linux, OS Linux\r\n'),
+(26, 'Kelebihan dan Kekurangan Linux yang Anda Perlu Ketahui', 'Operating System Linux telah hadir sejak awal 1990 dan hingga saat ini, OS Linux sangat umum ditemukan di server dikarenakan keamanan dan performanya. Mari kita bahas apa saja kekurangan dan kelebihdan dari OS ini?', 449, 'Daniel Tri', '2021-11-04', 'OS Linux, Performa, OS Gratis, Kelebihan Linux, Kekurangan Linux'),
 (27, '9 Manfaat Penulisan Artikel Pada Web Pribadi', 'mengunggah konten artikel pada website pada dasarnya sangat penting. Ada banyak sekali manfaat yang bisa Anda dapatkan dengan melakukan hal tersebut.Pada artikel ini kami akan membahas 9 manfaat penulisan artikel pada website pribadi', 1003, 'Daniel Tri', '2021-11-09', 'penulisan artikel, website pribadi, visitor website, traffic, social media, SEO'),
-(28, 'Cara Promosi Online yang Menarik dan Efektif', 'Berkembangnya dunia digital juga mempengaruhi cara melakukan promosi. Ada banyak cara yang bisa dilakukan, mari kita simak satu per satu metode yang dapat digunakan untuk melakukan promosi online pada artikel ini.', 1068, 'Daniel Tri', '2021-11-12', 'cara promosi online yang menarik, digital marketing, promosi online'),
+(28, 'Cara Promosi Online Yang Menarik dan Efektif', 'Berkembangnya dunia digital juga mempengaruhi cara melakukan promosi. Ada banyak cara yang bisa dilakukan, mari kita simak satu per satu metode yang dapat digunakan untuk melakukan promosi online pada artikel ini.', 1068, 'Daniel Tri', '2021-11-12', 'cara promosi online yang menarik, digital marketing, promosi online'),
 (29, 'Daftar Aplikasi Customer Service yang Mudah Digunakan', 'Aplikasi customer service merupakan sebuah perangkat lunak yang dipakai untuk memudahkan proses dalam pengaduan layanan dari pelanggan atau customer. Aplikasi ini dapat secara signifikan meningkatkan kualitas customer service itu sendiri serta meningkatkan branding pada perusahaan anda. Mari simak Aplikasi customer service yang dapat utilisasi saat ini.', 1065, 'Daniel Tri', '2021-11-18', 'Aplikasi Customer Service'),
 (30, '11 Aplikasi Meeting Online yang Memiliki Fitur Lengkap', 'Aplikasi meeting online saat ini telah berkembang dengan pesat. Seiring kemajuan teknologi dan kebutuhan atas dasar pandemi COVID-19, banyak developer yang mengembangkan aplikasi meeting online, mari simak 11 Aplikasi meeting online yang memiliki fitur terlengkap saat ini.', 1066, 'Daniel Tri', '2021-11-20', 'Online meeting, Zoom, Whatsapp, Google Meet, Skype, Slack, Cisco Webex, Jitsi Meet, Go To Meeting, Zoho Meeting, Bluejeans Video Converencing, Online Meeting Webinars'),
 (31, 'Apa Manfaat Website dalam Bisnis Online Secara Menyeluruh', 'Seiring perkembangan jaman, teknik marketing dan branding pun telah bergeser ke digital. Perusahaan berlomba - lomba untuk membuat website perusahaan mereka. Namun apa manfaat website dalam bisnis online yang bisa menjadi salah satu pertimbangan untuk dilakukan? Mari simak pembahasannya disini.', 801, 'Daniel Tri', '2021-11-22', 'Manfaat website dalam bisnis online, hemat waktu dan biaya, SEO'),
 (32, 'Kenapa Website Saya Sepi Pengunjung', 'Penyebab website sepi pengunjung bisa terjadi akibat berbagai macam hal, seperti pemilihan topik yang kurang sesuai, pemilihan audiens yang tidak tepat, ataupun kualitas SEO yang minim.', 833, 'Daniel Tri', '2021-11-26', 'penyebab website sepi pengunjung, SEO'),
 (33, 'Inilah Rekomendasi Aplikasi Penting Manajemen Perusahaan', 'Secara fundamental, teknologi dibuat dan dikembangkan untuk mempermudah manusia dan perusahaan. Pada artikel ini, kami akan membahas sejumlah aplikasi penting untuk manajemen perusahaan.', 731, 'Daniel Tri', '2021-11-24', 'aplikasi penting manajemen perusahaan, aplikasi, ERP, absensi, payroll, hris, accounting'),
-(34, 'Media Interaktif Berbasis Web Terbaik', 'Penggunaan media interaktif berbasis web ternyata begitu bermanfaat bagi klien. Dengan masa pandemi COVID-19 menyerang, salah satu opsi untuk menurunkan tingkan penyebarannya adalah untuk belajar secara digital. Media interaktif dapat membantu kita untuk melakukan hal tersebut dengan pengalaman semirip mungkin dengan pembelajaran tatap muka.', 857, 'Daniel Tri', '2021-11-27', 'media interaktif berbasis web');
+(34, 'Media Interaktif Berbasis Web Terbaik', 'Penggunaan media interaktif berbasis web ternyata begitu bermanfaat bagi klien. Dengan masa pandemi COVID-19 menyerang, salah satu opsi untuk menurunkan tingkan penyebarannya adalah untuk belajar secara digital. Media interaktif dapat membantu kita untuk melakukan hal tersebut dengan pengalaman semirip mungkin dengan pembelajaran tatap muka.', 857, 'Daniel Tri', '2021-11-27', 'media interaktif berbasis web'),
+(35, 'Cara Meningkatkan Traffic Web dengan Cepat', 'Salah satu hal, untuk bisa mendapatkan uang yang banyak dari sebuah website itu adalah memiliki pengunjung yang banyak. Terdapat banyak cara untuk meningkatkan traffic untuk website anda, pada artikel ini kami akan membahas beberapa metode tersebut.', 799, 'Daniel Tri', '2021-12-01', 'cara meningkatkan traffic web, SEO, traffic website'),
+(36, 'Tren Masa Kini, Penggunaan Teknologi AI dalam Web Development', 'Teknologi Artificial Intelligence yang saat ini sedang marak dibicarakan merupakan suatu produk teknologi dimana sebuah mesin atau komputer memiliki kemampuan untuk belajar. Pada artikel ini, kami akan membahas bagaimana AI dapat membantu dalam web development.', 700, 'Daniel Tri', '2021-12-02', 'AI dalam web development, Artificial Intelligence, Web development');
 
 -- --------------------------------------------------------
 
@@ -132,7 +133,9 @@ INSERT INTO `blog_tag` (`id`, `blog_id`, `tags_id`) VALUES
 (35, 31, 4),
 (36, 32, 1),
 (38, 33, 3),
-(39, 34, 3);
+(39, 34, 3),
+(40, 35, 1),
+(41, 36, 2);
 
 -- --------------------------------------------------------
 
@@ -144,9 +147,9 @@ CREATE TABLE `comment` (
   `id` int(255) NOT NULL,
   `blog_id` int(255) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `website` varchar(50) NOT NULL,
+  `website` varchar(50) DEFAULT NULL,
   `comment` text NOT NULL,
-  `created_on` datetime NOT NULL
+  `created_on` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -154,22 +157,25 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`id`, `blog_id`, `name`, `website`, `comment`, `created_on`) VALUES
-(1, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(2, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(3, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(4, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(5, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(6, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(7, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(8, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(9, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(10, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(11, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(12, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(13, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(14, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(15, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
-(16, 25, 'Test', 'Test', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26');
+(1, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(2, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(3, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(4, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(5, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(6, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(7, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(8, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(9, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(10, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(11, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(12, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(13, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(14, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(15, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(16, 25, 'Test', 'https://test.com', 'Terima kasih atas ilmunya, saya percaya pembahasan ini dapat membantu banyak  orang untuk membuat website mereka lebih baik lagi.', '2021-11-08 15:43:26'),
+(17, 25, 'Daniel Tri', 'https://test.com', 'as;dlfkjas ;dflkjasd; flkajs ;dflkasdf', '0000-00-00 00:00:00'),
+(18, 25, 'Daniel Tri', 'https://test.com', 'Hallo, saya telah bergerak di bidang ini.', '2021-12-03 07:45:06'),
+(19, 16, 'Daniel Tri', '', 'Saya suka artikel seperti ini.', '2021-12-03 07:55:25');
 
 -- --------------------------------------------------------
 
@@ -231,19 +237,19 @@ ALTER TABLE `tags`
 -- AUTO_INCREMENT for table `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `blog_tag`
 --
 ALTER TABLE `blog_tag`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tags`
